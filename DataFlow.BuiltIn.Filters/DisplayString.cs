856e7b1f7b8c5f3049287fa1b75b975665fb92d7
Tim <tim.cassens@gmail.com>
2012-04-12 23:36:42 +0200

    Add ToString and DisplayString builtin filters

---
+++ DataFlow.BuiltIn.Filters/DisplayString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataFlow.Model.Filters;
+
+namespace DataFlow.BuiltIn.Filters
+{
+	[Filter(DisplayName = "Display String")]
+	public class DisplayString : IFilter
+	{
+		[FilterInput]
+		public string Text { get; set; }
+
+		public void Run()
+		{
+			Console.WriteLine("\"{0}\"", Text);
+		}
+	}
+}
---
+++ DataFlow.BuiltIn.Filters/ToString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataFlow.Model.Filters;
+
+namespace DataFlow.BuiltIn.Filters
+{
+	[Filter(DisplayName = "Convert to String")]
+	public class ToString : IFilter
+	{
+		[FilterInput]
+		public object Value { get; set; }
+
+		[FilterOutput]
+		public string Result { get; private set; }
+
+		public void Run()
+		{
+			Result = Value.ToString();
+		}
+	}
+}
